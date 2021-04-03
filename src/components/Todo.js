132c9faba91d3147e7fb25723287908d2d447c92
Pratik Shahi <shahipr0@gmail.com>
2021-04-03 17:54:30 +0530

    added todo component

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+function Todo() {
+  return (
+    <div className="card">
+      <h2>TITLE</h2>
+      <div className="action">
+        <button className="btn">DELETE</button>
+      </div>
+    </div>
+  );
+}
+export default Todo;
