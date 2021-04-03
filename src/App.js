342a27ec66ba2a44b8370a65108551a6cd174e14
Pratik Shahi <shahipr0@gmail.com>
2021-04-03 18:13:53 +0530

    added modal and backdrop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
 function App() {
   return (
     <div>
       <h1>My todos</h1>
       <Todo title="my title" />
       <Todo title="my title 2" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,12 @@
 function Todo(props) {
+  function deleteHandler() {}
   return (
     <div className="card">
       <h2>{props.title}</h2>
       <div className="action">
-        <button className="btn">DELETE</button>
+        <button className="btn" onClick={deleteHandler}>
+          DELETE
+        </button>
       </div>
     </div>
   );
